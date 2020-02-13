c9b691f83b2a25785ffd997f9343174f50e60bc9
ocdman <ocdman2@gmail.com>
2020-02-13 16:48:47 +0800

    add go-cmp requirement to go.mod & add getConfigFilePath func to main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module v2ray.com/core
 
+require github.com/google/go-cmp v0.2.0
+
 go 1.13
--- main/main.go
+++ main/main.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"v2ray.com/core"
+	"v2ray.com/core/common/platform"
 )
 
 var (
@@ -11,6 +17,51 @@ var (
 	format     = flag.String("format", "json", "Format of input file.")
 )
 
+func fileExists(file string) bool {
+	info, err := os.Stat(file)
+	return err == nil && !info.IsDir()
+}
+
+func getConfigFilePath() string {
+	if len(*configFile) > 0 {
+		return *configFile
+	}
+
+	if workingDir, err := os.Getwd(); err == nil {
+		configFile := filepath.Join(workingDir, "config.json")
+		if fileExists(configFile) {
+			return configFile
+		}
+	}
+
+	if configFile := platform.GetConfigurationPath(); fileExists(configFile) {
+		return configFile
+	}
+
+	return ""
+}
+
+func startV2Ray() {
+	// configFile := getConfigFilePath()
+	getConfigFilePath()
+}
+
+func printVersion() {
+	version := core.VersionStatement()
+	for _, s := range version {
+		fmt.Println(s)
+	}
+}
+
 func main() {
 	flag.Parse()
+
+	printVersion()
+
+	if *version {
+		fmt.Println("return")
+		return
+	}
+
+	startV2Ray()
 }
